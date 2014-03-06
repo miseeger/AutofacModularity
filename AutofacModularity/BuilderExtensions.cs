894359cdf4c45a8637500f4db5a280ca54b72bb6
Michael Seeger <miseeger@gmx.de>
2014-03-06 06:13:50 +0100

    BuilderExtensions extended by methods using Assebmly.Load(). Version changed according to the supported Autofac version 3.3.0.

--- AutofacModularity/BuilderExtensions.cs
+++ AutofacModularity/BuilderExtensions.cs
@@ -7,68 +7,85 @@ using Autofac.Core;
 
 namespace AutofacModularity
 {
-    public static class BuilderExtensions
-    {
+	public static class BuilderExtensions
+	{
 
-        public static void RegisterAssemblyModulesFromFile(this ContainerBuilder builder, String loadFromFile)
-        {
-            builder.RegisterAssemblyModules<IModule>(Assembly.LoadFrom(loadFromFile));
-        }
-
-
-        public static void RegisterAssemblyModulesFromFile(this ContainerBuilder builder, String loadFromFile, 
-            Action<String> regCallbackAction)
-        {
-            builder.RegisterAssemblyModules<IModule>(Assembly.LoadFrom(loadFromFile));
-
-            if (regCallbackAction != null)
-            {
-                regCallbackAction.Invoke(loadFromFile);
-            }
-        }
+		public static void RegisterAssemblyModule(this ContainerBuilder builder, String assemblyName)
+		{
+			builder.RegisterAssemblyModules<IModule>(Assembly.Load(assemblyName));
+		}
 
 
-        public static void RegisterAssemblyModulesFromDirectory(this ContainerBuilder builder, String directory, 
-            String includingModulePrefix, String excludingModulePrefix)
-        {
-            var assemblyFileNames =
-                Directory.GetFiles(directory, "*.dll")
-                    .Where(
-                        f =>
-                            f.Contains(includingModulePrefix)
-                            && !f.Contains(excludingModulePrefix)
-                            && f.EndsWith(".dll")).ToList();
+		public static void RegisterAssemblyModule(this ContainerBuilder builder, String assemblyName, 
+			Action<String> regCallbackAction)
+		{
+			builder.RegisterAssemblyModules<IModule>(Assembly.Load(assemblyName));
 
-            foreach (var assemblyFileName in assemblyFileNames)
-            {
-                builder.RegisterAssemblyModulesFromFile(assemblyFileName);
-            }
-
-        }
+			if (regCallbackAction != null)
+			{
+				regCallbackAction.Invoke(assemblyName);
+			}
+		}
 
 
-        public static void RegisterAssemblyModulesFromDirectory(this ContainerBuilder builder, String directory,
-            String includingModulePrefix, String excludingModulePrefix, Action<String> regCallbackAction)
-        {
-            var assemblyFileNames =
-                Directory.GetFiles(directory, "*.dll")
-                    .Where(
-                        f =>
-                            f.Contains(includingModulePrefix)
-                            && !f.Contains(excludingModulePrefix)
-                            && f.EndsWith(".dll")).ToList();
+		public static void RegisterAssemblyModuleFromFile(this ContainerBuilder builder, String loadFromFile)
+		{
+			builder.RegisterAssemblyModules<IModule>(Assembly.LoadFrom(loadFromFile));
+		}
 
-            foreach (var assemblyFileName in assemblyFileNames)
-            {
-                builder.RegisterAssemblyModulesFromFile(assemblyFileName);
-                if (regCallbackAction != null)
-                {
-                    regCallbackAction.Invoke(assemblyFileName);
-                }
-            }
 
-        }
+		public static void RegisterAssemblyModuleFromFile(this ContainerBuilder builder, String loadFromFile, 
+			Action<String> regCallbackAction)
+		{
+			builder.RegisterAssemblyModules<IModule>(Assembly.LoadFrom(loadFromFile));
 
-    }
+			if (regCallbackAction != null)
+			{
+				regCallbackAction.Invoke(loadFromFile);
+			}
+		}
+
+
+		public static void RegisterAssemblyModulesFromDirectory(this ContainerBuilder builder, String directory, 
+			String includingModulePrefix, String excludingModulePrefix)
+		{
+			var assemblyFileNames =
+				Directory.GetFiles(directory, "*.dll")
+					.Where(
+						f =>
+							f.Contains(includingModulePrefix)
+								&& !f.Contains(excludingModulePrefix)
+								&& f.EndsWith(".dll")).ToList();
+
+			foreach (var assemblyFileName in assemblyFileNames)
+			{
+			builder.RegisterAssemblyModuleFromFile(assemblyFileName);
+			}
+		}
+
+
+		public static void RegisterAssemblyModulesFromDirectory(this ContainerBuilder builder, String directory,
+			String includingModulePrefix, String excludingModulePrefix, Action<String> regCallbackAction)
+		{
+			var assemblyFileNames =
+				Directory.GetFiles(directory, "*.dll")
+				.Where(
+					f =>
+						f.Contains(includingModulePrefix)
+							&& !f.Contains(excludingModulePrefix)
+							&& f.EndsWith(".dll")).ToList();
+
+			foreach (var assemblyFileName in assemblyFileNames)
+			{
+				builder.RegisterAssemblyModuleFromFile(assemblyFileName);
+
+				if (regCallbackAction != null)
+				{
+					regCallbackAction.Invoke(assemblyFileName);
+				}
+			}
+		}
+
+	}
 
 }
