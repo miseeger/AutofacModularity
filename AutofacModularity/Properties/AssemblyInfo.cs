22eccac90fe2d2809305e5e36dc87e449107d99e
Michael Seeger <miseeger@gmx.de>
2014-07-28 20:30:04 +0200

    Update to Autofac 3.5.0.0

--- AutofacModularity/Properties/AssemblyInfo.cs
+++ AutofacModularity/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AutofacModularity")]
-[assembly: AssemblyDescription("A small library to extend modularity with Autofac 3.3.0")]
+[assembly: AssemblyDescription("A small library to extend modularity with Autofac 3.5.0")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("AutofacModularity")]
@@ -28,4 +28,4 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("3.3.0.0")]
+[assembly: AssemblyVersion("3.5.0.0")]
