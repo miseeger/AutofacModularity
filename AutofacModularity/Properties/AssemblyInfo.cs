08359c96d37bced8bcf09f750eaf5ead7d47293c
Michael Seeger <miseeger@gmx.de>
2015-07-25 21:49:55 +0200

    Updated to 3.5.2

--- AutofacModularity/Properties/AssemblyInfo.cs
+++ AutofacModularity/Properties/AssemblyInfo.cs
@@ -10,11 +10,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AutofacModularity")]
-[assembly: AssemblyDescription("A small library to extend modularity with Autofac 3.5.0")]
+[assembly: AssemblyDescription("A small library to extend modularity with Autofac 3.5.2")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("AutofacModularity")]
-[assembly: AssemblyCopyright("Copyright 2013 - 2014")]
+[assembly: AssemblyCopyright("Copyright 2013 - 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -28,4 +28,4 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("3.5.0.0")]
+[assembly: AssemblyVersion("3.5.2.0")]
